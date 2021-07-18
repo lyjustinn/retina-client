9afbb6487d38e70c956f9204df1ad73ba0ccd52c
lyjustnn <lyjustinn@users.noreply.github.com>
2021-07-18 13:52:06 -0400

    setup routes, initialize layout component

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface LayoutProps {
+
+}
+
+const Layout: React.FC<LayoutProps> = ({}) => {
+    return (
+        <div>layout</div>
+    );
+}
+export default Layout;
--- src/components/routing/Routes.tsx
+++ src/components/routing/Routes.tsx
@@ -6,6 +6,17 @@ const Routes: React.FC = () => {
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={() => <div>main page</div>}/>
+                <Route exact path="/explore" component={() => <div>explore</div>}/>
+                <Route exact path="/search/:query" component={() => <div>search</div>}/>
+                <Route exact path="/collection/:id" component={() => <div>collection</div>}/>
+                <Route exact path="/photo/:id" component={() => <div>photo</div>} />
+                <Route exact path="/user/:id" component={() => <div>user profile</div>}/>
+
+                {/* Routes that require auth, redirect to 404 if invalid */}
+                <Route exact path="/edit/profile/:id" component={() => <div>edit profile</div>}/>
+                <Route exact path="/upload/photo" component={() => <div>upload photo</div>}/>
+                <Route exact path="/edit/photo/:id" component={() => <div>edit photo</div>}/>
+                <Route path="/" component={() => <div>404</div>}/>
             </Switch>
         </BrowserRouter>
     );
