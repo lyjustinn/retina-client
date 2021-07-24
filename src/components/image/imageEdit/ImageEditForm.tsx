394000733f65a56e30ad2074578a287b4bfeb3fa
lyjustnn <lyjustinn@users.noreply.github.com>
2021-07-24 17:03:25 -0400

    Create image edit form

---
+++ src/components/image/imageEdit/ImageEditForm.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+
+interface ImageEditFormProps {
+
+}
+
+const ImageEditForm: React.FC<ImageEditFormProps> = ({}) => {
+    const [input, setInput] = useState<{ [key:string] : string}>({
+        name: "",
+        description: ""
+    });
+
+    const handleChange = ( e : React.ChangeEvent<HTMLInputElement>) => {
+        const {value, name} = e.target;
+
+        setInput( prev => {
+            let copy = {...prev}
+            copy[name] = value;
+
+            return copy;
+        })
+    }
+
+    const handleDelete = (e : React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        
+        // send delete request
+    } 
+
+    const handleSubmit = (e : React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        
+
+        // send update request
+    }
+
+    return (
+        <Form>
+            <Form.Control onChange={handleChange} value={input.name} name="name" type="text"/>
+            <Form.Control onChange={handleChange} value={input.description} name="description" type="text"/>
+            <button onClick={handleDelete}>Delete</button>
+            <button onClick={handleSubmit}>Submit</button>
+        </Form>
+
+    );
+}
+export default ImageEditForm;
