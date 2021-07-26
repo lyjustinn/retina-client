37a29473906704edcb105ed90b3c1ffb03190605
lyjustnn <lyjustinn@users.noreply.github.com>
2021-07-26 00:30:43 -0400

    Login page styling

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 import { AuthRequest } from '../../types/userTypes';
 import { login } from '../../util/user/userService';
 
@@ -14,6 +15,7 @@ const Login: React.FC<LoginProps> = ({}) => {
         username: "",
         password: ""
     });
+    const [validate, setValidate] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {value, name} = e.target;
@@ -25,10 +27,13 @@ const Login: React.FC<LoginProps> = ({}) => {
         })
     }
 
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault()
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
 
         console.log(e.target);
+
+        setValidate(true);
 
         if (!input.username || !input.password) return;
 
@@ -52,15 +57,24 @@ const Login: React.FC<LoginProps> = ({}) => {
     }
 
     return (
-        <div className="w-50">
-            <Form>
-                <Form.Control value={input["username"]} onChange={handleChange} name="username" type="email" placeholder="Enter your email"/>
-                <Form.Control value={input["password"]} onChange={handleChange} name="password" type="password" placeholder="Password"/>
-                <button onClick={handleClick}>
+        <Container className="d-flex flex-column justify-content-center align-items-center h-100 w-50 retina-text-dark">
+            <h3 >Login</h3>
+            <Form noValidate className="w-50" validated={validate} onSubmit={handleSubmit}>
+                <Form.Group className="my-2">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control required value={input["username"]} onChange={handleChange} name="username" type="email" placeholder="Enter your email"/>
+                    <Form.Control.Feedback type="invalid">Please Enter Your Email</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="my-2">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control required value={input["password"]} onChange={handleChange} name="password" type="password" placeholder="Password"/>
+                    <Form.Control.Feedback type="invalid">Please Enter Your Password</Form.Control.Feedback>
+                </Form.Group>
+                <button className="w-100 retina-btn-dark my-2" type="submit">
                     Submit
                 </button>
             </Form>
-        </div>
+        </Container>
     );
 }
 export default Login;
