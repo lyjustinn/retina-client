9b56211e68d54e307262b289fc372f440461e670
lyjustnn <lyjustinn@users.noreply.github.com>
2021-07-25 23:38:30 -0400

    Clean up css files, add validation and styling to sign up page

--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 import { NewUser } from '../../types/userTypes';
 import { createUser } from '../../util/user/userService';
 
@@ -16,6 +17,7 @@ const Signup: React.FC<SignupProps> = ({}) => {
         firstName: "",
         lastName: ""
     });
+    const [validate, setValidate] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {value, name} = e.target;
@@ -27,8 +29,11 @@ const Signup: React.FC<SignupProps> = ({}) => {
         })
     }
 
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault()
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        setValidate(true)
 
         console.log(e.target);
 
@@ -53,17 +58,32 @@ const Signup: React.FC<SignupProps> = ({}) => {
     }
 
     return (
-        <div className="w-50">
-            <Form>
-                <Form.Control value={input["username"]} onChange={handleChange} name="username" type="email" placeholder="Enter your email"/>
-                <Form.Control value={input["firstName"]} onChange={handleChange} name="firstName" type="text" placeholder="Please enter your first name"/>
-                <Form.Control value={input["lastName"]} onChange={handleChange} name="lastName" type="text" placeholder="Please enter your last name"/>
-                <Form.Control value={input["password"]} onChange={handleChange} name="password" type="password" placeholder="Password"/>
-                <button onClick={handleClick}>
-                    Submit
-                </button>
+        <Container className="d-flex flex-column justify-content-center align-items-center h-100 w-50">
+            <h3>Sign Up</h3>
+            <Form noValidate className="w-50" validated={validate} onSubmit={handleSubmit}>
+                <Form.Group className="my-2">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control required value={input["username"]} onChange={handleChange} name="username" type="email" placeholder="Email"/>
+                    <Form.Control.Feedback type="invalid">Please Enter Your Email.</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="my-2">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control required value={input["firstName"]} onChange={handleChange} name="firstName" type="text" placeholder="First Name"/>
+                    <Form.Control.Feedback type="invalid">Please Enter Your First Name</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="my-2">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control required value={input["lastName"]} onChange={handleChange} name="lastName" type="text" placeholder="Last Name"/>
+                    <Form.Control.Feedback type="invalid">Please Enter Your Last Name</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="my-2">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control required value={input["password"]} onChange={handleChange} name="password" type="password" placeholder="Password"/>
+                    <Form.Control.Feedback type="invalid">Please Enter Your Password</Form.Control.Feedback>
+                </Form.Group>
+                <button className="w-100 my-2">Sign Up</button>
             </Form>
-        </div>
+        </Container>
     );
 }
 export default Signup;
