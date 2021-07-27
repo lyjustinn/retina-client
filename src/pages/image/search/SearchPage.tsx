12308a98260e77c46357406421c923a333eb8c57
lyjustnn <lyjustinn@users.noreply.github.com>
2021-07-27 00:16:43 -0400

    Setup header for search page results

--- src/pages/image/search/SearchPage.tsx
+++ src/pages/image/search/SearchPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../../../components/layout/Layout';
 import { match, RouteComponentProps } from 'react-router';
-import SearchResults from '../../../components/image/search/SearchResults';
+import SearchResults from '../../../components/image/SearchResults';
 
 interface MatchParams {
     query: string
