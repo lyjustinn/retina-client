adb2589a75a016e89d32cab0a99405929d753173
lyjustinn <87103438+lyjustinn@users.noreply.github.com>
2021-07-31 17:06:33 -0400

    Styling for header, footer, and landing page

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface FooterProps {
 
@@ -6,8 +7,9 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({}) => {
     return (
-        <footer>
-            this is my footer
+        <footer className="retina-bg-dark py-3 px-4 retina-text-light d-flex justify-content-between">
+            <span>Built by <a className="retina-text-blue text-decoration-none" href="https://github.com/lyjustinn">Justin Ly</a></span>
+            <span>Photo taken by <a className="retina-text-blue text-decoration-none" href="https://www.pexels.com/photo/architectural-photography-of-city-buildings-2341282/">Aleksander Parasic</a></span>
         </footer>
     );
 }
--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -8,7 +8,10 @@ const LandingPage: React.FC = ({}) => {
 
     return (
         <Layout param="temp" user={null}>
-            <div>this is the landing page</div>
+            <div className="w-100 h-100 hero d-flex justify-content-center flex-column" style={{backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('${process.env.REACT_APP_CDN_URL ?? ""}/hero.jpg')`}}>
+                <h1 className="m-0 text-center w-100 retina-text-light">Retina</h1>
+                <h4 className="m-0 text-center w-100 retina-text-light">Open Source Image Sharing Platform</h4>
+            </div>
         </Layout>
     );
 }
