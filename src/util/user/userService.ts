40ac4b607c7e72fc0162cc25ed29c9ac6601b954
lyjustinn <87103438+lyjustinn@users.noreply.github.com>
2021-07-31 20:26:13 -0400

    Refactor API services to use generic API fetch functions

--- src/util/user/userService.ts
+++ src/util/user/userService.ts
@@ -1,27 +1,12 @@
-import { User, NewUser, AuthRequest, UpdateRequest } from '../../types/userTypes';
+import { User, NewUser, AuthRequest, UpdateRequest, UserProfile } from '../../types/userTypes';
 import Cookies from 'universal-cookie';
+import { getRequest, bodyRequest } from '../createFetch';
 
 const cookies = new Cookies();
 
 export const createUser = async (newUser : NewUser) => {
     const body = JSON.stringify(newUser);
-    
-    const fetchOptions = {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: body
-    }
-
-    const url: string = process.env.REACT_APP_API_URL ?? "";
-
-    if (!url) throw new Error("invalid API url");
-
-    const response = await fetch(url + "/api/user/", fetchOptions);
-    if (!response.ok) throw new Error(`Fetch failed, status code ${response.status}`);
-
-    return true;
+    return bodyRequest("/api/user/", body);
 }
 
 export const login = async(authRequest: AuthRequest) => {
@@ -57,89 +42,22 @@ export const login = async(authRequest: AuthRequest) => {
 }
 
 export const getCurrentUser = async () : Promise<User> => {
-    const jwt : string = cookies.get("authToken");
-    const url: string = process.env.REACT_APP_API_URL ?? "";
-
-    const fetchOptions = {
-        method: 'get',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + jwt
-        }
-    }
-
-    if (!url || !jwt) throw new Error("invalid API url");
-
-    const response = await fetch(url + "/api/user/current", fetchOptions);
-
-    if (!response.ok) throw new Error(`Fetch failed, status code ${response.status}`);
-
-    return response.json();
+    return getRequest<User>("/api/user/current", true);
 }
 
 export const getUser = async (id : number) : Promise<User> => {
-
-    const url: string = process.env.REACT_APP_API_URL ?? "";
-
-    if (!url) throw new Error("invalid API url");
-
-    const response = await fetch(url + "/api/user/" + id);
-
-    if (!response.ok) throw new Error(`Fetch failed, status code ${response.status}`);
-
-    return response.json();
+    return getRequest<User>("/api/user/" + id);
 }
 
 export const updateUser = async (id : number, data : UpdateRequest) : Promise<boolean> => {
     const body = JSON.stringify(data);
-    
-    const jwt : string = cookies.get("authToken");
-    const fetchOptions = {
-        method: 'put',
-        headers: {
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + jwt
-        },
-        body: body
-    }
-
-    const url: string = process.env.REACT_APP_API_URL ?? "";
-
-    if (!url) return false;
-
-    const response = await fetch(url + "/api/user/" + id, fetchOptions);
-    return response.ok;
+    return bodyRequest("/api/user/" + id, body, true);
 }
 
-export const getUserProfile = async (id : number) => {
-    const url: string = process.env.REACT_APP_API_URL ?? "";
-
-    if (!url) throw new Error("invalid API url");
-
-    const response = await fetch(url + "/api/user/images/" + id);
-
-    if (!response.ok) throw new Error(`Fetch failed, status code ${response.status}`);
-
-    return response.json();
+export const getUserProfile = async (id : number) : Promise<UserProfile> => {
+    return getRequest<UserProfile>("/api/user/images/" + id);
 }
 
-export const getCurrentUserProfile = async () => {
-    const jwt : string = cookies.get("authToken");
-    const url: string = process.env.REACT_APP_API_URL ?? "";
-
-    const fetchOptions = {
-        method: 'get',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + jwt
-        }
-    }
-
-    if (!url || !jwt) throw new Error("invalid API url");
-
-    const response = await fetch(url + "/api/user/current/images", fetchOptions);
-
-    if (!response.ok) throw new Error(`Fetch failed, status code ${response.status}`);
-
-    return response.json();
+export const getCurrentUserProfile = async () : Promise<UserProfile> => {
+    return getRequest<UserProfile>("/api/user/current/images", true);
 }
